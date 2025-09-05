6403bc6bdc20823b5043c29cdda2710e06151cfd
srinivaspatnak15 <srinivaspatnaik1574@gmail.com>
2025-09-05 18:21:27 +0530

    hi

--- Food-Delivery-main/frontend/src/context/StoreContext.jsx
+++ Food-Delivery-main/frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-main1.onrender.com/";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
   const [user, setUser] = useState(null); // ✅ store user info
