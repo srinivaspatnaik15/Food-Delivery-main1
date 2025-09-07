7f65767da670a077a8d81e90fbf2a65d119f0b64
srinivaspatnak15 <srinivaspatnaik1574@gmail.com>
2025-09-07 18:15:58 +0530

    gt

--- Food-Delivery-main/frontend/vite.config.js
+++ Food-Delivery-main/frontend/vite.config.js
@@ -1,10 +1,15 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // 🔥 allows refresh in dev
+  },
+  preview: {
+    historyApiFallback: true, // 🔥 allows refresh in prod
+  },
   build: {
-    outDir: "dist",
+    outDir: "dist", // make sure build goes to dist
   },
 });
