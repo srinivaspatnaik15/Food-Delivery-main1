4d39f67e6fdfdabf31a35446acf243c0440b63f7
srinivaspatnak15 <srinivaspatnaik1574@gmail.com>
2025-09-07 15:48:57 +0530

    un

--- Food-Delivery-main/frontend/vite.config.js
+++ Food-Delivery-main/frontend/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ✅ ensures correct asset paths after deploy
-})
+  build: {
+    outDir: "dist",
+  },
+});
