73dd54032b6dd40d5a149a086b0198225a3c00b9
srinivaspatnak15 <srinivaspatnaik1574@gmail.com>
2025-09-07 15:02:13 +0530

    dr

--- Food-Delivery-main/backend/middleware/auth.js
+++ Food-Delivery-main/backend/middleware/auth.js
@@ -1,18 +1,21 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = async (req, res, next) => {
-  const { token } = req.headers;
-  if (!token) {
-    return res.json({ success: false, message: "Not Authorized Login Again" });
+  const authHeader = req.headers["authorization"]; // lowercase in Express
+  if (!authHeader) {
+    return res.json({ success: false, message: "Not Authorized, Login Again" });
   }
+
   try {
+    const token = authHeader.split(" ")[1]; // remove "Bearer"
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
     req.body.userId = token_decode.id;
     next();
   } catch (error) {
     console.log(error);
-    res.json({success:false,message:"Error"});
+    res.json({ success: false, message: "Invalid or expired token" });
   }
 };
+
 export default authMiddleware;
 
