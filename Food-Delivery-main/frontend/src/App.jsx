93fe47bf7ffe5f27ab36cae5ba106458591f5f05
srinivaspatnak15 <srinivaspatnaik1574@gmail.com>
2025-09-07 15:22:40 +0530

    hl

--- Food-Delivery-main/frontend/src/App.jsx
+++ Food-Delivery-main/frontend/src/App.jsx
@@ -5,18 +5,17 @@ import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Verify from "./pages/Verify/Verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 
-
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
-      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
       <div className="app">
         <ToastContainer />
         <Navbar setShowLogin={setShowLogin} />
@@ -24,7 +23,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
-          <Route path="/verify" element={<Verify />} />
           <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
